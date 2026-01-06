3888005633597315f4db8daac12039eb9fab1430
getdukka <getdukka@gmail.com>
2026-01-06 11:32:39 +0000

    Correction du problème overflow mobile

--- src/app/admin/edit/[id]/page.tsx
+++ src/app/admin/edit/[id]/page.tsx
@@ -4,7 +4,8 @@ import { notFound } from 'next/navigation'
 
 export const dynamic = 'force-dynamic'
 
-export default async function EditAuctionPage({ params }: { params: { id: string } }) {
+export default async function EditAuctionPage(props: { params: Promise<{ id: string }> }) {
+    const params = await props.params
     const supabase = await createClient()
     const { data: auction } = await supabase
         .from('auctions')
